e4cd56b1596f0ca851ef4017e66e7ec53db67ef4
Artem Kovalchuk <kovart.dev@gmail.com>
2023-03-31 21:01:01 +0300

    fix Erc721NonUniqueTokensModule module

--- src/analyzer/modules/non-unique-tokens.ts
+++ src/analyzer/modules/non-unique-tokens.ts
@@ -68,87 +68,90 @@ class Erc721NonUniqueTokensModule extends AnalyzerModule {
       }
     }
 
-    // Check for duplicate URI
-    let duplicatedTokenIdsByUri = new Map<string, string[]>();
-    for (const [tokenId, uri] of tokenUriByTokenId) {
-      const tokenIds = duplicatedTokenIdsByUri.get(uri) || [];
-      tokenIds.push(tokenId);
-      duplicatedTokenIdsByUri.set(uri, tokenIds);
-    }
-    duplicatedTokenIdsByUri = new Map(
-      [...duplicatedTokenIdsByUri].filter((entry) => entry[1].length > 1),
-    );
-    if (duplicatedTokenIdsByUri.size >= MIN_NUMBER_OF_DUPLICATE_TOKENS) {
-      detected = true;
-      metadata = {
-        duplicationType: 'uri',
-        duplicatedItems: [...duplicatedTokenIdsByUri.entries()].map(([uri, tokenIds]) => ({
-          uri,
-          tokenIds,
-        })),
-      };
-      return;
-    }
-
-    const metadataByTokenId = new Map<string, object | null>();
-
-    // Parse URI with Base64 encoding
-    for (const [tokenId, uri] of tokenUriByTokenId) {
-      if (isBase64(uri)) {
-        const metadata = parseBase64(uri);
-        metadataByTokenId.set(tokenId, metadata);
+    // Trick to be able to quit the function, while executing the final code
+    await (async () => {
+      // Check for duplicate URI
+      let duplicatedTokenIdsByUri = new Map<string, string[]>();
+      for (const [tokenId, uri] of tokenUriByTokenId) {
+        const tokenIds = duplicatedTokenIdsByUri.get(uri) || [];
+        tokenIds.push(tokenId);
+        duplicatedTokenIdsByUri.set(uri, tokenIds);
       }
-    }
+      duplicatedTokenIdsByUri = new Map(
+        [...duplicatedTokenIdsByUri].filter((entry) => entry[1].length > 1),
+      );
+      if (duplicatedTokenIdsByUri.size >= MIN_NUMBER_OF_DUPLICATE_TOKENS) {
+        detected = true;
+        metadata = {
+          duplicationType: 'uri',
+          duplicatedItems: [...duplicatedTokenIdsByUri.entries()].map(([uri, tokenIds]) => ({
+            uri,
+            tokenIds,
+          })),
+        };
+        return;
+      }
 
-    // Fetch non Base64-encoded URI
-    if (metadataByTokenId.size !== tokenUriByTokenId.size) {
-      const entries = [...tokenUriByTokenId]
-        // Filter out already processed tokens
-        .filter(([tokenId]) => !metadataByTokenId.has(tokenId))
-        // Normalize uri
-        .map(([tokenId, uri]) => [tokenId, normalizeMetadataUri(uri)])
-        // Filter out URI with null value after normalization
-        .filter(([, uri]) => uri) as [string, string][];
+      const metadataByTokenId = new Map<string, object | null>();
 
-      for (const batch of chunk(entries, 5)) {
-        try {
-          const metadataArr = await retry(() =>
-            Promise.all(batch.map(([, uri]) => memo('axios.get', [uri], () => axios.get(uri)))),
-          );
-          metadataArr.forEach((metadata, i) => metadataByTokenId.set(entries[i][0], metadata));
-        } catch (e) {
-          // Backend error?
-          Logger.error(e);
-          return;
+      // Parse URI with Base64 encoding
+      for (const [tokenId, uri] of tokenUriByTokenId) {
+        if (isBase64(uri)) {
+          const metadata = parseBase64(uri);
+          metadataByTokenId.set(tokenId, metadata);
         }
       }
-    }
 
-    // Check for metadata duplication
-    let duplicatedTokenIdsByMetadata = new Map<string, string[]>();
-    for (const [tokenId, metadata] of metadataByTokenId) {
-      if (!metadata) continue;
-      const str = JSON.stringify(metadata);
-      const tokenIds = duplicatedTokenIdsByMetadata.get(tokenId) || [];
-      tokenIds.push(tokenId);
-      duplicatedTokenIdsByMetadata.set(str, tokenIds);
-    }
-    duplicatedTokenIdsByMetadata = new Map(
-      [...duplicatedTokenIdsByMetadata].filter((entry) => entry[1].length > 1),
-    );
+      // Fetch non Base64-encoded URI
+      if (metadataByTokenId.size !== tokenUriByTokenId.size) {
+        const entries = [...tokenUriByTokenId]
+          // Filter out already processed tokens
+          .filter(([tokenId]) => !metadataByTokenId.has(tokenId))
+          // Normalize uri
+          .map(([tokenId, uri]) => [tokenId, normalizeMetadataUri(uri)])
+          // Filter out URI with null value after normalization
+          .filter(([, uri]) => uri) as [string, string][];
 
-    if (duplicatedTokenIdsByMetadata.size >= MIN_NUMBER_OF_DUPLICATE_TOKENS) {
-      detected = true;
-      metadata = {
-        duplicationType: 'metadata',
-        duplicatedItems: [...duplicatedTokenIdsByMetadata.entries()].map(
-          ([metadata, tokenIds]) => ({
-            tokenIds,
-            metadata,
-          }),
-        ),
-      };
-    }
+        for (const batch of chunk(entries, 5)) {
+          try {
+            const metadataArr = await retry(() =>
+              Promise.all(batch.map(([, uri]) => memo('axios.get', [uri], () => axios.get(uri)))),
+            );
+            metadataArr.forEach((metadata, i) => metadataByTokenId.set(entries[i][0], metadata));
+          } catch (e) {
+            // Backend error?
+            Logger.error(e);
+            return;
+          }
+        }
+      }
+
+      // Check for metadata duplication
+      let duplicatedTokenIdsByMetadata = new Map<string, string[]>();
+      for (const [tokenId, metadata] of metadataByTokenId) {
+        if (!metadata) continue;
+        const str = JSON.stringify(metadata);
+        const tokenIds = duplicatedTokenIdsByMetadata.get(tokenId) || [];
+        tokenIds.push(tokenId);
+        duplicatedTokenIdsByMetadata.set(str, tokenIds);
+      }
+      duplicatedTokenIdsByMetadata = new Map(
+        [...duplicatedTokenIdsByMetadata].filter((entry) => entry[1].length > 1),
+      );
+
+      if (duplicatedTokenIdsByMetadata.size >= MIN_NUMBER_OF_DUPLICATE_TOKENS) {
+        detected = true;
+        metadata = {
+          duplicationType: 'metadata',
+          duplicatedItems: [...duplicatedTokenIdsByMetadata.entries()].map(
+            ([metadata, tokenIds]) => ({
+              tokenIds,
+              metadata,
+            }),
+          ),
+        };
+      }
+    })();
 
     context[NON_UNIQUE_TOKENS_MODULE_KEY] = { detected, metadata };
   }
