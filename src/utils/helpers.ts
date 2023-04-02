c4f6dda4761888f889b99ecdb595861476bad71e
Artem Kovalchuk <kovart.dev@gmail.com>
2023-04-02 21:23:56 +0300

    move providersQueue helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,6 @@
 import { TransactionEvent } from 'forta-agent';
 import { Contract, ethers, providers, utils } from 'ethers';
+import { queue } from 'async';
 
 import Logger from './logger';
 import { CreatedContract, TokenStandard } from '../types';
@@ -268,4 +269,82 @@ export function isCid(str: string) {
   return /^(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})$/.test(
     str,
   );
+}
+
+export type ProvidersQueueResult<P, T> = {
+  running: () => number;
+  finish: () => Promise<void>;
+  kill: () => void;
+  push: (task: P) => Promise<void>;
+};
+
+export function providersQueue<P, T>(
+  handle: (task: P, provider: T) => Promise<unknown>,
+  providers: T[],
+): ProvidersQueueResult<P, T> {
+  if (!providers.length) throw new Error('No providers');
+
+  let counter = 0;
+  let goodProviders = [...providers];
+  let availableProviders = [...providers];
+  let unfinishedTasks: P[] = [];
+
+  const queueObject = queue<{
+    task: P;
+    provider: T;
+  }>((task, callback) => {
+    retry(() => handle(task.task, task.provider), { wait: 30 * 1000, attempts: 3 })
+      .then(() => callback())
+      .catch((e) => callback(e));
+  }, providers.length);
+
+  async function push(task: P) {
+    unfinishedTasks = [...unfinishedTasks, task];
+
+    let provider: T;
+
+    // wait until provider is released
+    while (true) {
+      if (goodProviders.length === 0) {
+        throw new Error('No good providers any more');
+      }
+
+      provider = availableProviders[counter % availableProviders.length];
+      availableProviders = availableProviders.filter((p) => p !== provider);
+      if (provider) {
+        counter++;
+        break;
+      }
+      await delay(5);
+    }
+
+    queueObject.push({ task, provider }, (err) => {
+      if (err) {
+        console.error('Excluding provider due to max number of attempts', err);
+        goodProviders = goodProviders.filter((p) => p !== provider);
+        availableProviders = availableProviders.filter((p) => p !== provider);
+        queueObject.concurrency = goodProviders.length;
+      } else {
+        availableProviders.push(provider);
+        unfinishedTasks = unfinishedTasks.filter((t) => t !== task);
+      }
+    });
+  }
+
+  async function finish() {
+    if (queueObject.running()) {
+      await queueObject.drain();
+    }
+
+    while (unfinishedTasks.length > 0) {
+      await delay(500);
+    }
+  }
+
+  return {
+    push: push,
+    finish: finish,
+    kill: () => queueObject.kill(),
+    running: () => queueObject.running(),
+  };
 }
--- tests/utils/utils.ts
+++ tests/utils/utils.ts
@@ -1,7 +1,6 @@
 import { providers } from 'ethers';
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
-import { queue } from 'async';
 
 dayjs.extend(duration);
 
@@ -45,82 +44,4 @@ export async function retry<T>(
       await delay(wait);
     }
   }
-}
-
-export type ProvidersQueueResult<P, T> = {
-  running: () => number;
-  finish: () => Promise<void>;
-  kill: () => void;
-  push: (task: P) => Promise<void>;
-};
-
-export function providersQueue<P, T>(
-  handle: (task: P, provider: T) => Promise<unknown>,
-  providers: T[],
-): ProvidersQueueResult<P, T> {
-  if (!providers.length) throw new Error('No providers');
-
-  let counter = 0;
-  let goodProviders = [...providers];
-  let availableProviders = [...providers];
-  let unfinishedTasks: P[] = [];
-
-  const queueObject = queue<{
-    task: P;
-    provider: T;
-  }>((task, callback) => {
-    retry(() => handle(task.task, task.provider), { wait: 30 * 1000, attempts: 3 })
-      .then(() => callback())
-      .catch((e) => callback(e));
-  }, providers.length);
-
-  async function push(task: P) {
-    unfinishedTasks = [...unfinishedTasks, task];
-
-    let provider: T;
-
-    // wait until provider is released
-    while (true) {
-      if (goodProviders.length === 0) {
-        throw new Error('No good providers any more');
-      }
-
-      provider = availableProviders[counter % availableProviders.length];
-      availableProviders = availableProviders.filter((p) => p !== provider);
-      if (provider) {
-        counter++;
-        break;
-      }
-      await delay(5);
-    }
-
-    queueObject.push({ task, provider }, (err) => {
-      if (err) {
-        console.log('Excluding provider due to max number of attempts', err);
-        goodProviders = goodProviders.filter((p) => p !== provider);
-        availableProviders = availableProviders.filter((p) => p !== provider);
-        queueObject.concurrency = goodProviders.length;
-      } else {
-        availableProviders.push(provider);
-        unfinishedTasks = unfinishedTasks.filter((t) => t !== task);
-      }
-    });
-  }
-
-  async function finish() {
-    if (queueObject.running()) {
-      await queueObject.drain();
-    }
-
-    while (unfinishedTasks.length > 0) {
-      await delay(500);
-    }
-  }
-
-  return {
-    push: push,
-    finish: finish,
-    kill: () => queueObject.kill(),
-    running: () => queueObject.running(),
-  };
 }
