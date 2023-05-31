7b0ceff122f17fddb5f2ebf9da2cbfdfff66f8c8
Artem Kovalchuk <kovart.dev@gmail.com>
2023-05-31 16:40:40 +0300

    update stats interval

--- src/agent.ts
+++ src/agent.ts
@@ -130,7 +130,7 @@ const provideHandleBlock = (data: DataContainer): HandleBlock => {
         await data.detector.wait();
       }
 
-      if (blockEvent.blockNumber % 10 == 0) data.detector.logStats();
+      if (blockEvent.blockNumber % 100 == 0) data.detector.logStats();
     }
 
     data.previousBlock = blockEvent.block;
