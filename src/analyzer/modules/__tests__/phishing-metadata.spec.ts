7fa25664b8f4041189dd746ea0f9efe6aae67f3c
Artem Kovalchuk <kovart.dev@gmail.com>
2023-06-18 17:58:50 +0300

    implement phishing checks in NFT description

--- src/analyzer/modules/__tests__/phishing-metadata.spec.ts
+++ src/analyzer/modules/__tests__/phishing-metadata.spec.ts
@@ -1,15 +1,24 @@
+import axios from 'axios';
 import { MockEthersProvider } from 'forta-agent-tools/lib/test';
+import AxiosMockAdapter from 'axios-mock-adapter';
 
 import { autoAddress } from './__utils__/helpers';
-
 import PhishingMetadataModule from '../phishing-metadata';
 import Memoizer from '../../../utils/cache';
 import { TokenContract, TokenStandard } from '../../../types';
 import { ModuleAnalysisResult } from '../../types';
-import { erc20Iface } from '../../../contants';
+import { erc1155Iface, erc20Iface, erc721Iface } from '../../../contants';
+import { normalizeMetadataUri } from '../../../utils/helpers';
+import DataStorage from '../../../storage';
+
+// make tests so they don't wait and retry
+jest.mock('../../../utils/helpers', () => ({
+  ...jest.requireActual('../../../utils/helpers'),
+  retry: (fn: Function) => fn(),
+}));
 
 describe('PhishingMetadata', () => {
-  describe('ERC20', () => {
+  describe('ERC-20', () => {
     let result!: ModuleAnalysisResult<PhishingMetadataModule>;
 
     const mockToken = {
@@ -98,6 +107,158 @@ describe('PhishingMetadata', () => {
     it('should detect phishing for "$ 123.32 go site.cc (TOKEN)"', async () => {
       await run(['$ 123.32 go site.cc', 'TOKEN']);
       expect(result.detected).toStrictEqual(true);
+    });
+  });
+
+  describe('NFT', () => {
+    let result!: ModuleAnalysisResult<PhishingMetadataModule>;
+    let tokenCount = 0;
+
+    const mockEthersProvider = new MockEthersProvider();
+    const indicator = new PhishingMetadataModule();
+
+    const mockAxios = new AxiosMockAdapter(axios);
+
+    async function run(standard: TokenStandard, description: string) {
+      const context: { [key: string]: ModuleAnalysisResult<PhishingMetadataModule> } = {};
+
+      const token = {
+        address: autoAddress(),
+        deployer: autoAddress(),
+        blockNumber: 1234,
+        timestamp: 3e8,
+        type: standard,
+      } as TokenContract;
+
+      const blockTag = undefined;
+
+      const mockDataStorage = {
+        getErc721TransferEvents: jest.fn(),
+        getErc1155TransferSingleEvents: jest.fn(),
+        getErc1155TransferBatchEvents: jest.fn(),
+      } as unknown as jest.Mocked<DataStorage>;
+
+      const tokenId = `${tokenCount++}`;
+      const uri = `ipfs://test-uri-${tokenId}.json`;
+      const normalizedUri = normalizeMetadataUri(uri)!;
+
+      if (standard === TokenStandard.Erc721) {
+        mockDataStorage.getErc721TransferEvents.mockImplementation(async (contract) => {
+          if (contract != token.address) throw new Error('Wrong contract address');
+          return [{ tokenId: tokenId }] as any;
+        });
+
+        mockEthersProvider.addCallTo(token.address, blockTag!, erc721Iface, 'tokenURI', {
+          inputs: [tokenId],
+          outputs: [uri],
+        });
+      } else if (standard === TokenStandard.Erc1155) {
+        mockDataStorage.getErc1155TransferSingleEvents.mockImplementation(async (contract) => {
+          if (contract != token.address) throw new Error('Wrong contract address');
+          return [{ tokenId: tokenId }] as any;
+        });
+
+        mockDataStorage.getErc1155TransferBatchEvents.mockImplementation(async (contract) => {
+          if (contract != token.address) throw new Error('Wrong contract address');
+          return [{ ids: [tokenId] }] as any;
+        });
+
+        mockEthersProvider.addCallTo(token.address, blockTag!, erc1155Iface, 'uri', {
+          inputs: [tokenId],
+          outputs: [uri],
+        });
+      }
+
+      mockAxios.onGet(normalizedUri).reply(200, { description });
+
+      await indicator.scan({
+        token: token,
+        blockNumber: 0,
+        timestamp: 0,
+        transformer: null as any,
+        storage: mockDataStorage,
+        memoizer: new Memoizer(),
+        provider: mockEthersProvider as unknown as any,
+        context: context,
+      });
+
+      result = context[indicator.key];
+    }
+
+    describe('ERC721', () => {
+      it('should not detect description with just keywords', async () => {
+        await run(
+          TokenStandard.Erc721,
+          'You have received some rewards. Visit my site to claim them.',
+        );
+        expect(result.detected).toStrictEqual(false);
+      });
+
+      it('should not detect description with just a dollar value', async () => {
+        await run(TokenStandard.Erc721, 'You have received a $100 NFT');
+        expect(result.detected).toStrictEqual(false);
+      });
+
+      it('should not detect description with just a link', async () => {
+        await run(TokenStandard.Erc721, 'Some text. site.com');
+        expect(result.detected).toStrictEqual(false);
+      });
+
+      it('should detect description with a link and keywords', async () => {
+        await run(TokenStandard.Erc721, 'Some text. Visit site.com');
+        expect(result.detected).toStrictEqual(true);
+        await run(TokenStandard.Erc721, 'Claim rewards at site.com');
+        expect(result.detected).toStrictEqual(true);
+        await run(TokenStandard.Erc721, 'Claim rewards at site[.]com');
+        expect(result.detected).toStrictEqual(true);
+        await run(TokenStandard.Erc721, 'Free mint [this link](site.com)');
+        expect(result.detected).toStrictEqual(true);
+        await run(TokenStandard.Erc721, 'The NFT available at [this link](site.com)');
+        expect(result.detected).toStrictEqual(true);
+      });
+
+      it('should detect description with a link and a dollar value', async () => {
+        await run(TokenStandard.Erc721, 'You have received $100. Claim here: site.com');
+        expect(result.detected).toStrictEqual(true);
+      });
+    });
+
+    describe('ERC1155', () => {
+      it('should not detect description with just keywords', async () => {
+        await run(
+          TokenStandard.Erc1155,
+          'You have received some rewards. Visit my site to claim them.',
+        );
+        expect(result.detected).toStrictEqual(false);
+      });
+
+      it('should not detect description with just a dollar value', async () => {
+        await run(TokenStandard.Erc1155, 'You have received a $100 NFT');
+        expect(result.detected).toStrictEqual(false);
+      });
+
+      it('should not detect description with just a link', async () => {
+        await run(TokenStandard.Erc1155, 'Some text. site.com');
+        expect(result.detected).toStrictEqual(false);
+      });
+
+      it('should detect description with a link and keywords', async () => {
+        await run(TokenStandard.Erc1155, 'Some text. Visit site.com');
+        expect(result.detected).toStrictEqual(true);
+        await run(TokenStandard.Erc1155, 'Claim rewards at site.com');
+        expect(result.detected).toStrictEqual(true);
+        await run(TokenStandard.Erc1155, 'Claim rewards at site[.]com');
+        expect(result.detected).toStrictEqual(true);
+        await run(TokenStandard.Erc1155, 'Free mint [this link](site.com)');
+        expect(result.detected).toStrictEqual(true);
+        await run(TokenStandard.Erc1155, 'The NFT available at [this link](site.com)');
+        expect(result.detected).toStrictEqual(true);
+      });
+
+      it('should detect description with a link and a dollar value', async () => {
+        await run(TokenStandard.Erc1155, 'You have received $100. Claim here: site.com');
+        expect(result.detected).toStrictEqual(true);
+      });
     });
   });
 });
--- src/analyzer/modules/phishing-metadata.ts
+++ src/analyzer/modules/phishing-metadata.ts
@@ -1,32 +1,99 @@
 import { ethers } from 'ethers';
+import axios from 'axios';
 
-import { erc20Iface } from '../../contants';
-import { containsLink, retry } from '../../utils/helpers';
-import { AnalyzerModule, ModuleScanReturn, ScanParams } from '../types';
+import { erc1155Iface, erc20Iface, erc721Iface } from '../../contants';
+import {
+  containsLink,
+  isBase64,
+  normalizeMetadataUri,
+  parseBase64,
+  retry,
+} from '../../utils/helpers';
+import { AnalyzerModule, ModuleAnalysisResult, ModuleScanReturn, ScanParams } from '../types';
 import { normalizeText } from '../../utils/normalizer';
+import { TokenStandard } from '../../types';
+import Logger from '../../utils/logger';
 
 // This module analyzes the metadata of tokens for the presence of a link to a website.
 // If such a link is found, it may suggest a phishing attack, particularly in the case of a large airdrop.
 
 export const PHISHING_METADATA_MODULE_KEY = 'PhishingMetadata';
-export const PHISHING_KEYWORDS = ['visit', 'claim', 'activate', 'reward', 'free', 'mint'];
-export const PHISHING_PATTERNS = [
+export const PHISHING_NAME_KEYWORDS = [
+  'visit',
+  'claim',
+  'activate',
+  'reward',
+  'free',
+  'mint',
+  'gift',
+  'at',
+];
+export const PHISHING_NAME_PATTERNS = [
   // $ 3000
   /\$\s*\d+/,
   // $ aave.site
   /\$\s+(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,
 ];
+export const PHISHING_DESCRIPTION_KEYWORDS = [
+  'visit',
+  'claim',
+  'activate',
+  'reward',
+  'gift',
+  'free mint',
+  'coupon',
+  'check it',
+  'check out',
+  'available here',
+  'available at',
+];
+export const PHISHING_DESCRIPTION_PATTERNS = [
+  /* presence of NFT value */
+  /\$\s*\d+/,
+];
 
 export type PhishingModuleMetadata = {
-  name: string;
-  symbol: string;
+  name?: string;
+  symbol?: string;
+  descriptionByTokenId?: {
+    [tokenId: string]: string;
+  };
 };
 
 class PhishingMetadataModule extends AnalyzerModule {
   static Key = PHISHING_METADATA_MODULE_KEY;
 
   async scan(params: ScanParams): Promise<ModuleScanReturn> {
-    const { token, memoizer, provider, context } = params;
+    const { context } = params;
+
+    let detected = false;
+    let metadata: PhishingModuleMetadata | undefined = undefined;
+
+    context[PHISHING_METADATA_MODULE_KEY] = { detected, metadata };
+
+    const erc20Phishing = await this.scanErc20Phishing(params);
+    if (erc20Phishing.detected) {
+      context[PHISHING_METADATA_MODULE_KEY] = erc20Phishing;
+      return;
+    }
+
+    const erc721Phishing = await this.scanErc721Phishing(params);
+    if (erc721Phishing.detected) {
+      context[PHISHING_METADATA_MODULE_KEY] = erc721Phishing;
+      return;
+    }
+
+    const erc1155Phishing = await this.scanErc1155Phishing(params);
+    if (erc1155Phishing.detected) {
+      context[PHISHING_METADATA_MODULE_KEY] = erc1155Phishing;
+      return;
+    }
+  }
+
+  private async scanErc20Phishing(
+    params: ScanParams,
+  ): Promise<ModuleAnalysisResult<PhishingModuleMetadata>> {
+    const { token, memoizer, provider } = params;
 
     let detected = false;
     let metadata: PhishingModuleMetadata | undefined = undefined;
@@ -49,16 +116,11 @@ class PhishingMetadataModule extends AnalyzerModule {
     } catch {
       // not supported
     }
-    const words = [name, symbol]
-      .join(' ')
-      .replace(/\[.\]/g, '.')
-      .replace(/\[dot\]/g, '.')
-      .split(' ')
-      .map((v) => normalizeText(v.toLowerCase()));
 
+    const words = this.getWords([name, symbol].join(' '));
     if (words.find(containsLink)) {
       for (const word of words) {
-        if (PHISHING_KEYWORDS.includes(word)) {
+        if (PHISHING_NAME_KEYWORDS.includes(word)) {
           detected = true;
           break;
         }
@@ -66,22 +128,189 @@ class PhishingMetadataModule extends AnalyzerModule {
 
       if (!detected) {
         for (const text of [name, symbol]) {
-          detected = !!PHISHING_PATTERNS.find((pattern) => pattern.test(text));
+          detected = !!PHISHING_NAME_PATTERNS.find((pattern) => pattern.test(text));
           if (detected) break;
         }
       }
 
-      metadata = {
-        name,
-        symbol,
-      };
+      metadata = { name, symbol };
     }
 
-    // TODO Test Erc721 description
+    return { detected, metadata };
+  }
 
-    context[PHISHING_METADATA_MODULE_KEY] = { detected, metadata };
+  private async scanErc721Phishing(
+    params: ScanParams,
+  ): Promise<ModuleAnalysisResult<PhishingModuleMetadata>> {
+    const { token, memoizer, storage, provider } = params;
 
-    return {};
+    let detected = false;
+    let metadata: PhishingModuleMetadata | undefined = {};
+
+    const emptyResult: ModuleAnalysisResult<PhishingModuleMetadata> = { detected, metadata };
+
+    if (token.type !== TokenStandard.Erc721) return emptyResult;
+
+    const memo = memoizer.getScope(token.address);
+
+    if (memo.get('isMetadataProviderBroken')) return emptyResult;
+    const isTokenURISupported = memo.get<boolean>('isTokenURISupported');
+    if (isTokenURISupported != null && !isTokenURISupported) return emptyResult;
+
+    let tokenIdSet = new Set<string>();
+    for (const event of await storage.getErc721TransferEvents(token.address)) {
+      tokenIdSet.add(event.tokenId.toString());
+    }
+
+    // in order to optimize performance we only check each 50th token
+    const tokenIds = [...tokenIdSet].filter((v, index) => index % 50 === 0);
+
+    try {
+      const erc721Contract = new ethers.Contract(token.address, erc721Iface, provider);
+
+      for (const tokenId of tokenIds) {
+        const uri = await memo('tokenURI', [tokenId], () =>
+          retry<string>(() => erc721Contract.tokenURI(tokenId)),
+        );
+
+        const tokenMetadata = await this.getUriMetadata(uri, params);
+
+        if (!tokenMetadata) {
+          memo.set('isMetadataProviderBroken', true);
+          break;
+        }
+
+        const description: string = tokenMetadata.description;
+
+        if (this.isPhishingDescription(description)) {
+          detected = true;
+          metadata.descriptionByTokenId = metadata.descriptionByTokenId || {};
+          metadata.descriptionByTokenId[tokenId] = tokenMetadata?.description;
+        }
+      }
+    } catch (e) {
+      Logger.info('ERC721 tokenURI() is not supported:', token.address);
+      // tokenURI() not supported
+      memo.set('isTokenURISupported', false);
+    }
+
+    return { detected, metadata };
+  }
+
+  private async scanErc1155Phishing(
+    params: ScanParams,
+  ): Promise<ModuleAnalysisResult<PhishingModuleMetadata>> {
+    const { token, memoizer, storage, provider } = params;
+
+    let detected = false;
+    let metadata: PhishingModuleMetadata | undefined = {};
+
+    const emptyResult: ModuleAnalysisResult<PhishingModuleMetadata> = { detected, metadata };
+
+    if (token.type !== TokenStandard.Erc1155) return emptyResult;
+
+    const memo = memoizer.getScope(token.address);
+
+    if (memo.get('isMetadataProviderBroken')) return emptyResult;
+    const isTokenURISupported = memo.get<boolean>('isTokenURISupported');
+    if (isTokenURISupported != null && !isTokenURISupported) return emptyResult;
+
+    let tokenIdSet = new Set<string>();
+    for (const event of await storage.getErc1155TransferSingleEvents(token.address)) {
+      tokenIdSet.add(event.tokenId.toString());
+    }
+    for (const event of await storage.getErc1155TransferBatchEvents(token.address)) {
+      for (const tokenId of event.ids) {
+        tokenIdSet.add(tokenId.toString());
+      }
+    }
+
+    // in order to optimize performance we only check each 50th token
+    const tokenIds = [...tokenIdSet].filter((v, index) => index % 50 === 0);
+
+    try {
+      const erc1155Contract = new ethers.Contract(token.address, erc1155Iface, provider);
+
+      for (const tokenId of tokenIds) {
+        const uri = await memo('tokenURI', [tokenId], () =>
+          retry<string>(() => erc1155Contract.uri(tokenId)),
+        );
+
+        const tokenMetadata = await this.getUriMetadata(uri, params);
+
+        if (!tokenMetadata) {
+          memo.set('isMetadataProviderBroken', true);
+          break;
+        }
+
+        const description: string = tokenMetadata.description;
+
+        if (this.isPhishingDescription(description)) {
+          detected = true;
+          metadata.descriptionByTokenId = metadata.descriptionByTokenId || {};
+          metadata.descriptionByTokenId[tokenId] = tokenMetadata?.description;
+        }
+      }
+    } catch (e) {
+      Logger.info('ERC721 tokenURI() is not supported:', token.address);
+      // tokenURI() not supported
+      memo.set('isTokenURISupported', false);
+    }
+
+    return { detected, metadata };
+  }
+
+  private getWords(text: string) {
+    return text
+      .replace(/\[.\]/g, '.')
+      .replace(/\[dot\]/g, '.')
+      .split(' ')
+      .map((v) => normalizeText(v.toLowerCase()));
+  }
+
+  private normalizeText(text: string) {
+    return this.getWords(text).join(' ');
+  }
+
+  private isPhishingDescription(text: string) {
+    const description = this.normalizeText(text);
+
+    if (!containsLink(description)) return false;
+
+    for (const keyword of PHISHING_DESCRIPTION_KEYWORDS) {
+      if (description.includes(keyword)) return true;
+    }
+
+    for (const pattern of PHISHING_DESCRIPTION_PATTERNS) {
+      if (pattern.test(description)) return true;
+    }
+
+    return false;
+  }
+
+  private async getUriMetadata(uri: string, params: ScanParams): Promise<Record<any, any> | null> {
+    const { memoizer, token } = params;
+    const memo = memoizer.getScope(token.address);
+
+    let metadata: any | null = null;
+
+    if (isBase64(uri)) {
+      metadata = parseBase64(uri);
+    } else {
+      const url = normalizeMetadataUri(uri);
+      if (!url) return null;
+
+      try {
+        metadata = await memo('axios.get', [url], async () => {
+          const { data } = await retry(() => axios.get(url));
+          return data;
+        });
+      } catch (e) {
+        Logger.error('Metadata fetching error', { error: e });
+      }
+    }
+
+    return metadata;
   }
 }
 
