9a5ef65ce55902b59b651f2277164a4d7e9c7d83
Artem Kovalchuk <kovart.dev@gmail.com>
2023-06-26 12:59:40 +0300

    re-design MultipleOwners indicator to cover cases with dropped txs

---
+++ src/analyzer/modules/__tests__/multiple-owners.spec.ts
@@ -0,0 +1,579 @@
+import { MockEthersProvider } from 'forta-agent-tools/lib/test';
+import { ethers } from 'ethers';
+
+import { autoAddress } from './__utils__/helpers';
+import Erc721MultipleOwnersModule, {
+  Erc721MultipleOwnersModuleMetadata,
+  MULTIPLE_OWNERS_MODULE_KEY,
+} from '../multiple-owners';
+import { ModuleAnalysisResult } from '../../types';
+import { erc721Iface } from '../../../contants';
+import { SimplifiedTransaction, TokenContract, TokenStandard } from '../../../types';
+import DataStorage from '../../../storage';
+import { createAddress } from 'forta-agent-tools';
+
+describe('Erc721MultipleOwners', () => {
+  const indicator = new Erc721MultipleOwnersModule({
+    minDuplicatedTokens: 1,
+    minDuplicatedTokensFromSameSender: 1,
+  });
+
+  const mockStorage = {
+    getErc721TransferEvents: jest.fn(),
+  } as jest.MockedObject<DataStorage>;
+  const mockEthersProvider = new MockEthersProvider();
+
+  const token: TokenContract = {
+    address: autoAddress(),
+    deployer: autoAddress(),
+    blockNumber: 1234,
+    timestamp: 3e8,
+    type: TokenStandard.Erc721,
+  };
+
+  let context: { [key: string]: ModuleAnalysisResult<Erc721MultipleOwnersModuleMetadata> } = {};
+
+  beforeEach(() => {
+    mockStorage.getErc721TransferEvents.mockClear();
+    context = {};
+  });
+
+  function createTransaction(params: {
+    blockNumber: number;
+    index?: number;
+    hash?: string;
+  }): SimplifiedTransaction {
+    return {
+      timestamp: 0,
+      index: params.index ?? 0,
+      blockNumber: params.blockNumber,
+      to: token.address,
+      from: autoAddress(),
+      hash: params.hash || '0xHASH',
+      sighash: '0xSIGHASH',
+    };
+  }
+
+  it('should not detect if it is not ERC-721 token', async () => {
+    await indicator.scan({
+      provider: mockEthersProvider as unknown as ethers.providers.JsonRpcProvider,
+      storage: mockStorage,
+      blockNumber: 200,
+      context,
+      token: { ...token, type: TokenStandard.Erc1155 },
+      timestamp: 0,
+      transformer: null!,
+      memoizer: null!,
+    });
+
+    expect(mockStorage.getErc721TransferEvents).not.toBeCalled();
+    expect(context[MULTIPLE_OWNERS_MODULE_KEY].detected).toStrictEqual(false);
+  });
+
+  it('should not detect if there are no multiple owners', async () => {
+    const blockNumber = 222;
+    const address1 = autoAddress();
+    const address2 = autoAddress();
+    const address3 = autoAddress();
+
+    // Break the order so that the indicator sorts on its own
+    mockStorage.getErc721TransferEvents.mockResolvedValue([
+      {
+        from: address2,
+        to: address3,
+        tokenId: '0',
+        contract: token.address,
+        logIndex: 1,
+        transaction: createTransaction({ blockNumber, index: 1 }),
+      },
+      {
+        from: address1,
+        to: address2,
+        tokenId: '0',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber, index: 0 }),
+      },
+      {
+        from: address1,
+        to: address2,
+        tokenId: '1',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber, index: 0 }),
+      },
+      {
+        from: address2,
+        to: address3,
+        tokenId: '1',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber: blockNumber + 2 }),
+      },
+      {
+        from: address3,
+        to: address2,
+        tokenId: '0',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber: blockNumber + 1 }),
+      },
+    ]);
+
+    await indicator.scan({
+      provider: mockEthersProvider as unknown as ethers.providers.JsonRpcProvider,
+      storage: mockStorage,
+      blockNumber: blockNumber + 2,
+      context,
+      token,
+      timestamp: 0,
+      transformer: null!,
+      memoizer: null!,
+    });
+
+    expect(context[MULTIPLE_OWNERS_MODULE_KEY].detected).toStrictEqual(false);
+  });
+
+  it('should not detect duplication caused by dropped tx', async () => {
+    const blockNumber = 222;
+    const address1 = autoAddress();
+    const address2 = autoAddress();
+    const address3 = autoAddress();
+    const tokenId = '0';
+
+    mockStorage.getErc721TransferEvents.mockResolvedValue([
+      {
+        from: address1,
+        to: address2,
+        tokenId: tokenId,
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber, index: 0 }),
+      },
+      // Let's assume there is a dropped event here (address2 -> address3)
+      {
+        from: address3,
+        to: address2,
+        tokenId: tokenId,
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber: blockNumber + 2 }),
+      },
+    ]);
+
+    mockEthersProvider.addCallTo(token.address, blockNumber + 1, erc721Iface, 'ownerOf', {
+      inputs: [tokenId],
+      outputs: [address3],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber + 2, erc721Iface, 'ownerOf', {
+      inputs: [tokenId],
+      outputs: [address2],
+    });
+
+    await indicator.scan({
+      provider: mockEthersProvider as unknown as ethers.providers.JsonRpcProvider,
+      storage: mockStorage,
+      blockNumber: blockNumber + 2,
+      context,
+      token,
+      timestamp: 0,
+      transformer: null!,
+      memoizer: null!,
+    });
+
+    expect(context[MULTIPLE_OWNERS_MODULE_KEY].detected).toStrictEqual(false);
+  });
+
+  it('should detect if NFT was minted to multiple owners', async () => {
+    const blockNumber = 222;
+    const zeroAddress = ethers.constants.AddressZero;
+    const address1 = autoAddress();
+    const address2 = autoAddress();
+    const tokenId = '0';
+
+    mockStorage.getErc721TransferEvents.mockResolvedValue([
+      {
+        from: zeroAddress,
+        to: address1,
+        tokenId: tokenId,
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber }),
+      },
+      {
+        from: zeroAddress,
+        to: address2,
+        tokenId: tokenId,
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber: blockNumber + 1 }),
+      },
+    ]);
+
+    // Usually, in such spam, the owner is the account on which the first mint was made
+    mockEthersProvider.addCallTo(token.address, blockNumber, erc721Iface, 'ownerOf', {
+      inputs: [tokenId],
+      outputs: [address1],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber + 1, erc721Iface, 'ownerOf', {
+      inputs: [tokenId],
+      outputs: [address1],
+    });
+
+    await indicator.scan({
+      provider: mockEthersProvider as unknown as ethers.providers.JsonRpcProvider,
+      storage: mockStorage,
+      blockNumber: blockNumber + 100,
+      context,
+      token,
+      timestamp: 0,
+      transformer: null!,
+      memoizer: null!,
+    });
+
+    expect(context[MULTIPLE_OWNERS_MODULE_KEY].detected).toStrictEqual(true);
+  });
+
+  it('should detect if NFT was transferred multiple times to same owner', async () => {
+    const blockNumber = 222;
+    const address1 = autoAddress();
+    const address2 = autoAddress();
+    const address3 = autoAddress();
+    const tokenId = '0';
+
+    mockStorage.getErc721TransferEvents.mockResolvedValue([
+      {
+        from: address1,
+        to: address3,
+        tokenId: tokenId,
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber }),
+      },
+      {
+        from: address2,
+        to: address3,
+        tokenId: tokenId,
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber: blockNumber + 1 }),
+      },
+    ]);
+
+    mockEthersProvider.addCallTo(token.address, blockNumber, erc721Iface, 'ownerOf', {
+      inputs: [tokenId],
+      outputs: [address3],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber + 1, erc721Iface, 'ownerOf', {
+      inputs: [tokenId],
+      outputs: [address3],
+    });
+
+    await indicator.scan({
+      provider: mockEthersProvider as unknown as ethers.providers.JsonRpcProvider,
+      storage: mockStorage,
+      blockNumber: blockNumber + 1,
+      context,
+      token,
+      timestamp: 0,
+      transformer: null!,
+      memoizer: null!,
+    });
+
+    expect(context[MULTIPLE_OWNERS_MODULE_KEY].detected).toStrictEqual(true);
+  });
+
+  it('should detect if NFT was transferred to multiple owners in one tx', async () => {
+    const blockNumber = 222;
+    const address1 = autoAddress();
+    const address2 = autoAddress();
+    const address3 = autoAddress();
+    const tokenId = '0';
+
+    mockStorage.getErc721TransferEvents.mockResolvedValue([
+      {
+        from: ethers.constants.AddressZero,
+        to: address1,
+        tokenId: tokenId,
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber }),
+      },
+      {
+        from: address1,
+        to: address2,
+        tokenId: tokenId,
+        contract: token.address,
+        logIndex: 1,
+        transaction: createTransaction({ blockNumber }),
+      },
+      {
+        from: address1,
+        to: address3,
+        tokenId: tokenId,
+        contract: token.address,
+        logIndex: 2,
+        transaction: createTransaction({ blockNumber }),
+      },
+    ]);
+
+    await indicator.scan({
+      provider: mockEthersProvider as unknown as ethers.providers.JsonRpcProvider,
+      storage: mockStorage,
+      blockNumber: blockNumber,
+      context,
+      token,
+      timestamp: 0,
+      transformer: null!,
+      memoizer: null!,
+    });
+
+    expect(context[MULTIPLE_OWNERS_MODULE_KEY].detected).toStrictEqual(true);
+  });
+
+  it('should not detect if min number of duplicates is not satisfied', async () => {
+    let indicator = new Erc721MultipleOwnersModule({
+      minDuplicatedTokens: 3,
+      minDuplicatedTokensFromSameSender: 2,
+    });
+
+    const blockNumber = 222;
+
+    // 2 senders with 2 duplicated tokens
+    mockStorage.getErc721TransferEvents.mockResolvedValue([
+      {
+        from: createAddress('0x0'),
+        to: createAddress('0x1'),
+        tokenId: '0',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber }),
+      },
+      {
+        from: createAddress('0x0'),
+        to: createAddress('0x2'),
+        tokenId: '0',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber: blockNumber + 1 }),
+      },
+      {
+        from: createAddress('0x1'),
+        to: createAddress('0x2'),
+        tokenId: '1',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber }),
+      },
+      {
+        from: createAddress('0x1'),
+        to: createAddress('0x3'),
+        tokenId: '1',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber: blockNumber + 1 }),
+      },
+    ]);
+
+    mockEthersProvider.addCallTo(token.address, blockNumber, erc721Iface, 'ownerOf', {
+      inputs: ['0'],
+      outputs: [createAddress('0x1')],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber + 1, erc721Iface, 'ownerOf', {
+      inputs: ['0'],
+      outputs: [createAddress('0x1')],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber, erc721Iface, 'ownerOf', {
+      inputs: ['1'],
+      outputs: [createAddress('0x2')],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber + 1, erc721Iface, 'ownerOf', {
+      inputs: ['1'],
+      outputs: [createAddress('0x2')],
+    });
+
+    await indicator.scan({
+      provider: mockEthersProvider as unknown as ethers.providers.JsonRpcProvider,
+      storage: mockStorage,
+      blockNumber: blockNumber + 1,
+      context,
+      token,
+      timestamp: 0,
+      transformer: null!,
+      memoizer: null!,
+    });
+
+    expect(context[MULTIPLE_OWNERS_MODULE_KEY].detected).toStrictEqual(false);
+
+    // minDuplicatedTokensFromSameSender
+    // ----------------------------------------------
+
+    indicator = new Erc721MultipleOwnersModule({
+      minDuplicatedTokens: 999, // should not be satisfied
+      minDuplicatedTokensFromSameSender: 2,
+    });
+
+    // 1 sender, 2 duplicates
+    mockStorage.getErc721TransferEvents.mockResolvedValue([
+      {
+        from: createAddress('0x1'),
+        to: createAddress('0x2'),
+        tokenId: '0',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber }),
+      },
+      {
+        from: createAddress('0x1'),
+        to: createAddress('0x3'),
+        tokenId: '0',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber: blockNumber + 1 }),
+      },
+      {
+        from: createAddress('0x1'),
+        to: createAddress('0x2'),
+        tokenId: '1',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber: blockNumber + 2 }),
+      },
+      {
+        from: createAddress('0x1'),
+        to: createAddress('0x3'),
+        tokenId: '1',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber: blockNumber + 3 }),
+      },
+    ]);
+
+    mockEthersProvider.addCallTo(token.address, blockNumber, erc721Iface, 'ownerOf', {
+      inputs: ['0'],
+      outputs: [createAddress('0x2')],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber + 1, erc721Iface, 'ownerOf', {
+      inputs: ['0'],
+      outputs: [createAddress('0x2')],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber + 2, erc721Iface, 'ownerOf', {
+      inputs: ['1'],
+      outputs: [createAddress('0x2')],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber + 3, erc721Iface, 'ownerOf', {
+      inputs: ['1'],
+      outputs: [createAddress('0x2')],
+    });
+
+    await indicator.scan({
+      provider: mockEthersProvider as unknown as ethers.providers.JsonRpcProvider,
+      storage: mockStorage,
+      blockNumber: blockNumber + 3,
+      context,
+      token,
+      timestamp: 0,
+      transformer: null!,
+      memoizer: null!,
+    });
+
+    expect(context[MULTIPLE_OWNERS_MODULE_KEY].detected).toStrictEqual(true);
+
+    // minDuplicatedTokens
+    // ----------------------------------------------
+
+    indicator = new Erc721MultipleOwnersModule({
+      minDuplicatedTokens: 3,
+      minDuplicatedTokensFromSameSender: 999, // should not be satisfied
+    });
+
+    mockStorage.getErc721TransferEvents.mockResolvedValue([
+      {
+        from: createAddress('0x1'),
+        to: createAddress('0x2'),
+        tokenId: '0',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber }),
+      },
+      {
+        from: createAddress('0x3'),
+        to: createAddress('0x4'),
+        tokenId: '0',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber: blockNumber + 1 }),
+      },
+      {
+        from: createAddress('0x5'),
+        to: createAddress('0x6'),
+        tokenId: '1',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber }),
+      },
+      {
+        from: createAddress('0x7'),
+        to: createAddress('0x8'),
+        tokenId: '1',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber: blockNumber + 1 }),
+      },
+      {
+        from: createAddress('0x9'),
+        to: createAddress('0x10'),
+        tokenId: '2',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber }),
+      },
+      {
+        from: createAddress('0x11'),
+        to: createAddress('0x12'),
+        tokenId: '2',
+        contract: token.address,
+        logIndex: 0,
+        transaction: createTransaction({ blockNumber: blockNumber + 1 }),
+      },
+    ]);
+
+    mockEthersProvider.addCallTo(token.address, blockNumber, erc721Iface, 'ownerOf', {
+      inputs: ['0'],
+      outputs: [createAddress('0x2')],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber + 1, erc721Iface, 'ownerOf', {
+      inputs: ['0'],
+      outputs: [createAddress('0x2')],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber, erc721Iface, 'ownerOf', {
+      inputs: ['1'],
+      outputs: [createAddress('0x3')],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber + 1, erc721Iface, 'ownerOf', {
+      inputs: ['1'],
+      outputs: [createAddress('0x3')],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber, erc721Iface, 'ownerOf', {
+      inputs: ['2'],
+      outputs: [createAddress('0x5')],
+    });
+    mockEthersProvider.addCallTo(token.address, blockNumber + 1, erc721Iface, 'ownerOf', {
+      inputs: ['2'],
+      outputs: [createAddress('0x5')],
+    });
+
+    await indicator.scan({
+      provider: mockEthersProvider as unknown as ethers.providers.JsonRpcProvider,
+      storage: mockStorage,
+      blockNumber: blockNumber + 1,
+      context,
+      token,
+      timestamp: 0,
+      transformer: null!,
+      memoizer: null!,
+    });
+
+    expect(context[MULTIPLE_OWNERS_MODULE_KEY].detected).toStrictEqual(true);
+  });
+});
--- src/analyzer/modules/multiple-owners.ts
+++ src/analyzer/modules/multiple-owners.ts
@@ -1,62 +1,171 @@
-import { TokenStandard } from '../../types';
+import { ethers } from 'ethers';
+import { groupBy, max, maxBy, sortBy } from 'lodash';
+import { Erc721TransferEvent, TokenStandard } from '../../types';
 import { AnalyzerModule, ModuleScanReturn, ScanParams } from '../types';
+import { erc721Iface } from '../../contants';
 
 // Detect when single token in the ERC721 collection
 // has been transferred to multiple owners indicating fraudulent transfers.
 // Example: https://etherscan.io/nft/0x000386e3f7559d9b6a2f5c46b4ad1a9587d59dc3/2
 
 export const MULTIPLE_OWNERS_MODULE_KEY = 'Erc721MultipleOwners';
+export const MIN_DUPLICATED_TOKENS = 10;
+export const MIN_DUPLICATED_TOKENS_FROM_SAME_SENDER = 5;
+
+export type DuplicatedTransferEvent = {
+  from: string;
+  to: string;
+  txHash: string;
+};
 
 export type Erc721MultipleOwnersModuleMetadata = {
-  ownersByTokenId: { [tokenId: string]: string[] };
+  duplicatedTransfersByTokenId: { [tokenId: string]: DuplicatedTransferEvent[] };
 };
 
 export type Erc721MultipleOwnersModuleShortMetadata = {
   duplicatedTokenCount: number;
-  duplicatedTokenShortMap: { [tokenId: string]: string[] };
+  duplicatedTransferCount: number;
+  duplicatedTransferShortMap: { [tokenId: string]: DuplicatedTransferEvent[] };
 };
+
+// This module has been designed to allow situations in which the bot may have dropped transactions
 
 class Erc721MultipleOwnersModule extends AnalyzerModule {
   static Key = MULTIPLE_OWNERS_MODULE_KEY;
 
+  private minDuplicatedTokens: number;
+  private minDuplicatedTokensFromSameSender: number;
+
+  constructor(config?: {
+    minDuplicatedTokens?: number;
+    minDuplicatedTokensFromSameSender?: number;
+  }) {
+    super();
+
+    this.minDuplicatedTokens = config?.minDuplicatedTokens || MIN_DUPLICATED_TOKENS;
+    this.minDuplicatedTokensFromSameSender =
+      config?.minDuplicatedTokensFromSameSender || MIN_DUPLICATED_TOKENS_FROM_SAME_SENDER;
+  }
+
   async scan(params: ScanParams): Promise<ModuleScanReturn> {
-    const { token, context, storage } = params;
+    const { token, context, blockNumber, storage, provider } = params;
 
     let detected = false;
     let metadata: Erc721MultipleOwnersModuleMetadata | undefined = undefined;
 
-    if (token.type === TokenStandard.Erc721) {
-      const transferEvents = await storage.getErc721TransferEvents(token.address);
+    context[MULTIPLE_OWNERS_MODULE_KEY] = { detected, metadata };
 
-      // tokenId -> account[]
-      const ownersByTokenId = new Map<string, string[]>();
+    if (token.type !== TokenStandard.Erc721) return;
 
-      for (const event of transferEvents) {
-        const tokenId = event.tokenId.toString();
-        let holders = ownersByTokenId.get(tokenId) || [];
+    const duplicationsByTokenId = new Map<string, DuplicatedTransferEvent[]>();
 
-        // Remove the previous owner, but only once
-        const previousOwnerIndex = holders.indexOf(event.from);
-        if (previousOwnerIndex > -1) {
-          holders.splice(previousOwnerIndex, 1);
+    const transferEvents = (await storage.getErc721TransferEvents(token.address)).filter(
+      (e) => e.transaction.blockNumber <= blockNumber,
+    );
+    const eventsByBlock = groupBy(transferEvents, (e) => e.transaction.blockNumber);
+    const transferEntries = Object.entries(eventsByBlock);
+    // Sort by blockNumber
+    transferEntries.sort((a, b) => Number(a[0]) - Number(b[0]));
+
+    const append = <T, P>(map: Map<T, P[]>, key: T, value: P) => {
+      let arr = map.get(key);
+      if (!arr) {
+        arr = [];
+      }
+      arr.push(value);
+      map.set(key, arr);
+    };
+
+    // tokenId -> transfers[]
+    const allTransfersByTokenId = new Map<string, Erc721TransferEvent[]>();
+    for (const [block, transfers] of transferEntries) {
+      const blockNumber = Number(block);
+      const transfersByTokenId = groupBy(transfers, (e) => e.tokenId.toString());
+
+      for (const [tokenId, tokenTransfers] of Object.entries(transfersByTokenId)) {
+        // Sort by multiple fields; first transaction index then log index
+        const sortedTransfers = sortBy(tokenTransfers, [
+          (t) => t.transaction.index,
+          (t) => t.logIndex,
+        ]);
+
+        for (let i = 0; i < sortedTransfers.length; i++) {
+          const allTokenTransfers = allTransfersByTokenId.get(tokenId);
+          const currTokenTransfer = sortedTransfers[i];
+          const prevTokenTransfer = allTokenTransfers
+            ? allTokenTransfers[allTokenTransfers.length - 1]
+            : null;
+
+          if (prevTokenTransfer) {
+            if (prevTokenTransfer.to !== currTokenTransfer.from) {
+              // Detected an anomaly; multiple owners of the same token id
+
+              const isFirstTransferInBlock = i === 0;
+              if (isFirstTransferInBlock) {
+                // Check with blockchain data
+                const contract = new ethers.Contract(token.address, erc721Iface, provider);
+                const prevBlockchainOwner = (
+                  await contract.ownerOf(tokenId, {
+                    blockTag: blockNumber - 1,
+                  })
+                ).toLowerCase();
+                const currentBlockchainOwner = (
+                  await contract.ownerOf(tokenId, {
+                    blockTag: blockNumber,
+                  })
+                ).toLowerCase();
+
+                if (
+                  prevBlockchainOwner !== currTokenTransfer.from ||
+                  currentBlockchainOwner !== currTokenTransfer.to
+                ) {
+                  // We've confirmed detection with blockchain data
+
+                  append(duplicationsByTokenId, tokenId, {
+                    txHash: currTokenTransfer.transaction.hash,
+                    from: currTokenTransfer.from,
+                    to: currTokenTransfer.to,
+                  });
+                }
+              } else {
+                append(duplicationsByTokenId, tokenId, {
+                  txHash: currTokenTransfer.transaction.hash,
+                  from: currTokenTransfer.from,
+                  to: currTokenTransfer.to,
+                });
+              }
+            }
+          }
+
+          append(allTransfersByTokenId, tokenId, currTokenTransfer);
         }
+      }
+    }
 
-        // add new owner
-        holders.push(event.to);
-        ownersByTokenId.set(tokenId, holders);
+    const duplicatedTokenIdsBySender = new Map<string, number>();
+    for (const [tokenId, transfers] of duplicationsByTokenId) {
+      for (const transfer of transfers) {
+        const duplicatedTokens = duplicatedTokenIdsBySender.get(transfer.from) || 0;
+        duplicatedTokenIdsBySender.set(transfer.from, duplicatedTokens + 1);
+      }
+    }
+
+    const maxDuplicationPerSender: number = max([...duplicatedTokenIdsBySender.values()]) ?? 0;
+
+    if (
+      maxDuplicationPerSender >= this.minDuplicatedTokensFromSameSender ||
+      duplicationsByTokenId.size >= this.minDuplicatedTokens
+    ) {
+      detected = true;
+
+      const map: { [tokenId: string]: DuplicatedTransferEvent[] } = {};
+      for (const [tokenId, transfers] of duplicationsByTokenId) {
+        map[tokenId] = transfers;
       }
 
-      const duplicatedOwnersByTokenId: { [tokenId: string]: string[] } = {};
-      for (const [tokenId, owners] of ownersByTokenId) {
-        if (owners.length > 1) {
-          detected = true;
-          duplicatedOwnersByTokenId[tokenId] = owners;
-        }
-      }
-
-      if (detected) {
-        metadata = { ownersByTokenId: duplicatedOwnersByTokenId };
-      }
+      metadata = {
+        duplicatedTransfersByTokenId: map,
+      };
     }
 
     context[MULTIPLE_OWNERS_MODULE_KEY] = { detected, metadata };
@@ -65,16 +174,17 @@ class Erc721MultipleOwnersModule extends AnalyzerModule {
   simplifyMetadata(
     metadata: Erc721MultipleOwnersModuleMetadata,
   ): Erc721MultipleOwnersModuleShortMetadata {
-    const duplicatedTokenShortMap: { [tokenId: string]: string[] } = {};
-    Object.entries(metadata.ownersByTokenId)
+    const duplicatedTransferShortMap: { [tokenId: string]: DuplicatedTransferEvent[] } = {};
+    Object.entries(metadata.duplicatedTransfersByTokenId)
       .slice(0, 15)
-      .map(([tokenId, owners]) => {
-        duplicatedTokenShortMap[tokenId] = owners.slice(0, 5);
+      .map(([tokenId, transfers]) => {
+        duplicatedTransferShortMap[tokenId] = transfers.slice(0, 5);
       });
 
     return {
-      duplicatedTokenCount: Object.keys(metadata.ownersByTokenId).length,
-      duplicatedTokenShortMap: duplicatedTokenShortMap,
+      duplicatedTokenCount: Object.keys(metadata.duplicatedTransfersByTokenId).length,
+      duplicatedTransferCount: Object.values(metadata.duplicatedTransfersByTokenId).flat().length,
+      duplicatedTransferShortMap: duplicatedTransferShortMap,
     };
   }
 }
