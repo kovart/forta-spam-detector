da16053224b022fb391d033d21450c3523280fe6
Artem Kovalchuk <kovart.dev@gmail.com>
2023-06-29 17:45:48 +0300

    add script for publishing beta version

---
+++ build_beta.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Building beta version of the bot..."
+
+cp -f package.json package.json.temp
+cp -f forta.config.json forta.config.json.temp
+
+echo "Modifying package.json, forta.config.json, Dockerfile..."
+
+npm pkg set 'name'='spam-detector-beta'
+
+SOURCE_KEY="betaAgentId"
+DESTINATION_KEY="agentId"
+JSON=$(cat forta.config.json)
+SOURCE_VALUE=$(echo "$JSON" | jq -r ".$SOURCE_KEY")
+# Use jq to insert the source value into the destination key
+JSON=$(echo "$JSON" | jq --arg key "$DESTINATION_KEY" --arg value "$SOURCE_VALUE" '. + { ($key): $value }')
+# Write the updated JSON back to the file
+echo "$JSON" >forta.config.json
+
+npm run publish
+
+echo "Restoring original configs..."
+
+mv package.json.temp package.json
+mv forta.config.json.temp forta.config.json
