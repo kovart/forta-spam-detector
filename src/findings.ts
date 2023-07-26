0af301eea7fed357655203c7254e76a08b314ad4
Artem Kovalchuk <kovart.dev@gmail.com>
2023-07-26 14:24:05 +0300

    add dynamic confidence score to labels and metadata

--- src/findings.ts
+++ src/findings.ts
@@ -7,6 +7,13 @@ import ObservationTimeModule from './analyzer/modules/observation-time';
 import TokenImpersonation, {
   TokenImpersonationModuleMetadata,
 } from './analyzer/modules/token-impersonation';
+import AirdropModule, { AirdropModuleShortMetadata } from './analyzer/modules/airdrop';
+import PhishingMetadataModule, {
+  PhishingModuleMetadata,
+} from './analyzer/modules/phishing-metadata';
+import HighActivityModule, {
+  HighActivityModuleShortMetadata,
+} from './analyzer/modules/high-activity';
 
 const BASE_ALERT_ID = 'SPAM-TOKEN';
 const NEW_ALERT_ID = `${BASE_ALERT_ID}-NEW`;
@@ -30,23 +37,85 @@ const formatTriggeredModules = (analysis: AnalysisContext) => {
   return modules.join(', ');
 };
 
-export function createSpamNewFinding(token: Token, analysis: AnalysisContext) {
-  const labels = [
+export function getIndicators(analysis: AnalysisContext): string[] {
+  return Object.entries(analysis)
+    .filter((e) => e[1].detected && e[0] !== ObservationTimeModule.Key)
+    .map((e) => e[0]);
+}
+
+export function getConfidence(analysis: AnalysisContext): number {
+  const indicators = getIndicators(analysis);
+
+  let confidence = 0.55;
+
+  if (indicators.length == 3) {
+    confidence += 0.15;
+  } else if (indicators.length >= 4) {
+    confidence += 0.35;
+  }
+
+  const receivers =
+    (analysis[AirdropModule.Key].metadata as AirdropModuleShortMetadata)?.receiverCount ?? 0;
+
+  if (receivers >= 1000) {
+    confidence *= 1.2;
+  } else if (receivers >= 100) {
+    confidence *= 1.1;
+  }
+
+  const description: string =
+    Object.values(
+      (analysis[PhishingMetadataModule.Key].metadata as PhishingModuleMetadata)
+        ?.descriptionByTokenId || {},
+    )?.[0] || '';
+
+  // Too much for phishing?
+  if (description.length >= 2000) {
+    confidence *= 0.8;
+  }
+
+  const senders: number =
+    (analysis[HighActivityModule.Key].metadata as HighActivityModuleShortMetadata)?.senderCount ||
+    0;
+
+  if (senders >= 300) {
+    confidence *= 0.8;
+  }
+
+  return Math.min(1, confidence);
+}
+
+export function getLabels(token: Token, analysis: AnalysisContext, remove: boolean = false) {
+  const confidence = getConfidence(analysis);
+  const indicators = getIndicators(analysis);
+
+  return [
     Label.fromObject({
       label: 'Spam',
       entity: token.address,
-      remove: false,
+      remove: remove,
       entityType: EntityType.Address,
-      confidence: 0.7,
+      confidence: confidence,
+      metadata: {
+        indicators: JSON.stringify(indicators),
+      },
     }),
     Label.fromObject({
       label: 'Spammer',
       entity: token.deployer,
-      remove: false,
+      remove: remove,
       entityType: EntityType.Address,
-      confidence: 0.7,
+      confidence: confidence,
+      metadata: {
+        indicators: JSON.stringify(indicators),
+      },
     }),
   ];
+}
+
+export function createSpamNewFinding(token: Token, analysis: AnalysisContext) {
+  const confidence = getConfidence(analysis);
+  const labels = getLabels(token, analysis, false);
 
   return Finding.from({
     alertId: NEW_ALERT_ID,
@@ -59,6 +128,7 @@ export function createSpamNewFinding(token: Token, analysis: AnalysisContext) {
     labels: labels,
     addresses: [token.address, token.deployer],
     metadata: {
+      confidence: confidence.toString(),
       tokenAddress: token.address,
       tokenStandard: `ERC-${token.type}`,
       tokenDeployer: token.deployer,
@@ -84,6 +154,9 @@ export function createSpamUpdateFinding(
   const addedModules = diff.filter((m) => !prevModules.includes(m));
   const removedModules = diff.filter((m) => !currModules.includes(m));
 
+  const confidence = getConfidence(currAnalysis);
+  const labels = getLabels(token, currAnalysis, false);
+
   return Finding.from({
     alertId: UPDATE_ALERT_ID,
     name: 'Spam Token (Update)',
@@ -95,9 +168,10 @@ export function createSpamUpdateFinding(
       (removedModules.length > 0 ? ` Removed: ${removedModules.join(', ')}.` : ''),
     type: FindingType.Suspicious,
     severity: FindingSeverity.Low,
-    labels: [],
-    addresses: [],
+    labels: labels,
+    addresses: [token.address, token.deployer],
     metadata: {
+      confidence: confidence.toString(),
       tokenAddress: token.address,
       tokenStandard: `ERC-${token.type}`,
       tokenDeployer: token.deployer,
@@ -107,22 +181,7 @@ export function createSpamUpdateFinding(
 }
 
 export function createSpamRemoveFinding(token: Token, currAnalysis: AnalysisContext) {
-  const labels = [
-    Label.fromObject({
-      label: 'Spam',
-      entity: token.address,
-      remove: true,
-      entityType: EntityType.Address,
-      confidence: 0.7,
-    }),
-    Label.fromObject({
-      label: 'Spammer',
-      entity: token.deployer,
-      remove: true,
-      entityType: EntityType.Address,
-      confidence: 0.7,
-    }),
-  ];
+  const labels = getLabels(token, currAnalysis, true);
 
   return Finding.from({
     alertId: REMOVE_ALERT_ID,
